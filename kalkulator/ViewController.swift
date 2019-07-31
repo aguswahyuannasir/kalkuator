2b01c6dc15ea586a9bac8e7fed4a52b82c0db887
anas <aguswahyuannasir@gmail.com>
2019-07-31 12:52:55 +0700

    tes

--- kalkulator/ViewController.swift
+++ kalkulator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func number(_ sender: UIButton)
+    {
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
